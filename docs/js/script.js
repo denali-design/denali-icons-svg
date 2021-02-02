9ac78a300b6ca4de1e980f2e897ea23bea404309
Jon Kilroy <jkilroy@gmail.com>
2021-02-02 01:10:48 -0600

    Add ability to share preview (#3)

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,25 +1,53 @@
+(function() {
+  var html = document.getElementById('html');
+  var renderElm = document.createElement('div');
 
-var html = document.getElementById("html");
-var code = document.getElementById("code").contentWindow.document;
+  function parseUrl() {
+    var params = Qs.parse(document.location.search, { ignoreQueryPrefix: true });
+    if(params.src) {
+      var decompressed = LZString.decompressFromEncodedURIComponent(params.src);
+      html.value = decompressed;
+    }
+  }
 
-function compile() {
-    code.open();
-    code.writeln(
-      "<div style='height: 100%;' class='denali-default-theme flex align-items-center justify-content-center flex-column'>" +
-      html.value +
-      "</div>" +
-      "<style>" +
-      "@import url('css/denali.css'); @import url('css/denali-dark-theme.css'); body {background: transparent}</style>"
-    );
-    code.close();
-}
+  function initPreview() {
+    var previewElm = document.querySelector('#preview-container').contentWindow.document;
 
-document.body.addEventListener("keyup", compile);
+    var styleElm = document.createElement('style');
+    styleElm.innerHTML = '@import url("css/denali.css"); @import url("css/denali-dark-theme.css"); body {background: transparent}';
+    previewElm.querySelector('head').appendChild(styleElm);
 
-compile();
+    renderElm.id = 'render-container';
+    renderElm.className = 'denali-default-theme flex align-items-center justify-content-center flex-column';
+    renderElm.style = 'height: 100%';
+    previewElm.querySelector('body').appendChild(renderElm);
+  
+    document.body.addEventListener('keyup', updatePreview);
+  }
 
-function copyCode() {
-  html.select();
-  html.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-}
+  function updatePreview() {
+    renderElm.innerHTML = html.value; 
+  }
+
+  function copyCode() {
+    html.select();
+    html.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+  }
+
+  function initShareLink() {
+    var btn = document.querySelector('#share-link');
+    var clipboard = new ClipboardJS(btn, {
+      text: function(trigger) {
+        var url = new URL(document.location);
+        url.search = 'src=' + LZString.compressToEncodedURIComponent(html.value);
+        return url;
+      }
+    });
+  }
+
+  parseUrl();
+  initPreview();
+  updatePreview();
+  initShareLink();
+})();
